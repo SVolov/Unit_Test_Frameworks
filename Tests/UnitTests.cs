7a4f9bc9d53cc960403732847e0c23cfc1ada20f
Sergei Volovik <sergei.volovik@icloud.com>
2023-10-23 20:02:15 +0300

    add code

---
+++ Tests/UnitTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using TaskOne;
+using Unit_Test_Frameworks;
+
+namespace Tests
+{
+    public class UnitTests
+    {
+        [TestCase("1111", 0)]
+        [TestCase("ваппвппп", 0)]
+        [TestCase("dgkjdlfkg", 0)]
+        [TestCase("   ", 0)]
+        [TestCase("qqqwert12ывап", 3)]
+        [TestCase("", 0)]
+        [TestCase("<dfff>?/", 3)]
+        [TestCase("AasdFgRt", 0)]
+
+
+        public void CountEqualLatinLettersTest(string input, int expectedResult)
+        {
+            var actualResult = StringActions.CountEqualLatinLetters(input);
+            Assert.AreEqual(expectedResult, actualResult, "Error: result is wrong");
+        }
+
+        [TestCase("1111", 0)]
+        [TestCase("qqqqqqq", 0)]
+        [TestCase("ппппппп", 0)]
+        [TestCase("   ", 0)]
+        [TestCase("", 0)]
+        [TestCase("рпа123qwe", 9)]
+        [TestCase("<df>?/", 6)]
+
+        public void CountUnequalSymbolsTest(string input, int expectedResult)
+        {
+            var actualResult = StringActions.CountUnequalSymbols(input);
+            Assert.AreEqual(expectedResult, actualResult, "Error: result is wrong");
+        }
+
+        [TestCase("123456", 0)]
+        [TestCase("123dfg", 0)]
+        [TestCase("ппппппп", 0)]
+        [TestCase("123апрqwe", 0)]
+        [TestCase("qwer11234", 2)]
+        [TestCase("1111", 4)]
+        [TestCase("   ", 0)]
+        [TestCase("", 0)]
+        [TestCase("<df>?/", 0)]
+
+        public void CountEqualFiguresTest(string input, int expectedResult)
+        {
+            var actualResult = StringActions.CountEqualFigures(input);
+            Assert.AreEqual(expectedResult, actualResult, "Error: result is wrong");
+        }
+    }
+}
--- Unit_Test_Frameworks/Program.cs
+++ Unit_Test_Frameworks/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Formats.Asn1;
+using System.Linq.Expressions;
+using Unit_Test_Frameworks;
+
+namespace TaskOne
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write line, please");
+            string line = Console.ReadLine();
+
+            int count1 = StringActions.CountUnequalSymbols(line);
+            int count2 = StringActions.CountEqualLatinLetters(line);
+            int count3 = StringActions.CountEqualFigures(line);
+
+            Console.WriteLine("Unequal symbols in string : " + count1);
+            Console.WriteLine("Equal Latin letters in string :  " + count2);
+            Console.WriteLine("Equal figures in string : " + count3);
+        }
+    }       
+}
